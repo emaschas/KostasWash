84d4a520558fc3e50de050b773ed6127e3d566aa
Emmanuel Maschas <30336575+emaschas@users.noreply.github.com>
2018-03-14 17:44:32 +0100

    Update Washing.c

--- Washing.c
+++ Washing.c
@@ -5,105 +5,135 @@
 #include "KostasWash.h"
 #include "Washing.h"
 
+// Shared Variables
+// ----------------
+volatile uint8_t  WashState = NO_WASH;
+volatile uint8_t  RotationState = NO_ROTATION;
+
 //-----------------------------------------------------------------------------------
 // Rotation State Machine
 //-----------------------------------------------------------------------------------
+
+void StartRotation(void) { RotationState = RUN1_ROTATION; }
+void  StopRotation(void) { RotationState = STOP_ROTATION; }
+
 void RotationControl(void)
 {
-  static uint32_t  NextTime = 0;
-  if(millis() > NextTime && RotationState != NO_ROTATION)
+  static uint8_t   AfterPause;
+  if((countDown2() == 0 && RotationState != NO_ROTATION) || RotationState == STOP_ROTATION) 
   {
+    DisplayStatus();
     switch(RotationState)
     {
       case RUN1_ROTATION:
-        RELAY01_OFF();                    // Rotation direction 1
-        RELAY02_ON();                     // Start Motor
-        NextTime = millis()+seconds(3);   // Duration : 3 sec.
-        LCD_move(0,15);
-        LCD_putc('+');
-        RotationState = PAUSE1_ROTATION;
-      break;
-      case PAUSE1_ROTATION:
-        RELAY02_OFF();                    // Stop Motor
-        NextTime = millis()+seconds(1);   // Duration : 1 sec.
-        RotationState = RUN2_ROTATION;
-        LCD_move(0,15);
-        LCD_putc('o');
+        RELAY01_OFF();             // Rotation direction 1
+        RELAY02_ON();              // Start Motor
+        startTimer2(seconds(3));   // Duration : 3 sec.
+        RotationState = PAUSE_ROTATION;
+        AfterPause    = RUN2_ROTATION;
       break;
       case RUN2_ROTATION:
-        RELAY01_ON();                      // Rotation direction 2
-        RELAY02_ON();                      // Start Motor
-        NextTime = millis()+seconds(3);    // Duration : 3 sec.
-        RotationState = PAUSE2_ROTATION;
-        LCD_move(0,15);
-        LCD_putc('-');
+        RELAY01_ON();               // Rotation direction 2
+        RELAY02_ON();               // Start Motor
+        startTimer2(seconds(3));    // Duration : 3 sec.
+        RotationState = PAUSE_ROTATION;
+        AfterPause    = RUN1_ROTATION;
       break;
-      case PAUSE2_ROTATION:
-        RELAY02_OFF();                     // Stop Motor
-        NextTime = millis()+seconds(1);    // Duration : 1 sec.
-        RotationState = RUN1_ROTATION;
-        LCD_move(0,15);
-        LCD_putc('o');
+      case PAUSE_ROTATION:
+        RELAY02_OFF();              // Stop Motor
+        startTimer2(seconds(1));    // Duration : 1 sec.
+        RotationState = AfterPause;
       break;
       case STOP_ROTATION:
-        RELAY01_OFF();                     // Rotation direction off
-        RELAY02_OFF();                     // Stop Motor
+        RELAY01_OFF();              // Rotation direction off
+        RELAY02_OFF();              // Stop Motor
         RotationState = NO_ROTATION;
-        LCD_move(0,15);
-        LCD_putc('x');      
       break;
     }
   }
 }
+
 
 //-----------------------------------------------------------------------------------
 // Washing State Machine
 //-----------------------------------------------------------------------------------
+
+uint8_t NoWash(void) { return WashState == NO_WASH; }
+void StartWash(void) { WashState = START_WASH; }
+void AbortWash(void) { WashState = ABORT_WASH; }
+
 void WashControl(void)
 {
-  static uint32_t  NextTime = 0;
-  if(millis() > NextTime && WashState != NO_WASH)
+  static uint8_t  AfterPause;
+  if((countDown1() == 0 && WashState != NO_WASH) || WashState == ABORT_WASH)
   {
+    DisplayStatus();
     switch(WashState)
     {
       case START_WASH:
-        NextTime = millis()+seconds(2);
+        startTimer1(seconds(2));
         WashState = PRE_WASH;
-        LCD_move(0,0);
-        LCD_puts("Start   ");
       break;
       case PRE_WASH:
-        NextTime = millis()+minutes(1);  // Duration : 1 min.
-        WashState = MAIN_WASH;
-        RotationState = RUN1_ROTATION;
-        LCD_move(0,0);
-        LCD_puts("PreWash ");
+        StartRotation();
+        startTimer1(minutes(1));
+        WashState = PAUSE_WASH;
+        AfterPause = MAIN_WASH;
       break;
       case MAIN_WASH:
-        NextTime = millis()+minutes(1);  // Duration : 1 min.
-        WashState = RINCE;
-        RotationState = STOP_ROTATION;
-        LCD_move(0,0);
-        LCD_puts("Wash    ");
+        startTimer1(minutes(1));
+        WashState = PAUSE_WASH;
+        AfterPause = RINCE;
+        StopRotation();
       break;
       case RINCE:
-        NextTime = millis()+minutes(1);  // Duration : 1 min.
+        startTimer1(minutes(1));
         WashState = DRY;
-        LCD_move(0,0);
-        LCD_puts("Rince   ");
       break;
       case DRY:
-        NextTime = millis()+minutes(1);  // Duration : 1 min.
+        startTimer1(minutes(1));
         WashState = END_WASH;
-        LCD_move(0,0);
-        LCD_puts("Dry     ");
+      break;
+      case PAUSE_WASH:
+        startTimer1(seconds(2));
+        WashState = AfterPause;
       break;
       case END_WASH:
+      case ABORT_WASH:
         WashState = NO_WASH;
-        LCD_move(0,0);
-        LCD_puts("Waiting ");
       break;
     }
   }
 }
 
+//-----------------------------------------------------------------------------------
+// Display Status
+//-----------------------------------------------------------------------------------
+
+void DisplayStatus(void)
+{
+  char* WashText[] =
+  {
+    "No     ",
+    "Start  ",
+    "Prewash",
+    "Wash   ",
+    "Rince  ",
+    "Dry    ",
+    "Finish ",
+    "Pause  ",
+    "Abort  "
+  };
+  char* RotationText[] =
+  {
+    "No    ",
+    "Turn +",
+    "Turn -",
+    "Pause ",
+    "Stop  "
+  };
+  LCD_move(0,0);
+  LCD_puts(RotationText[RotationState]);
+  LCD_move(0,9);
+  LCD_puts(WashText[WashState]);
+}
